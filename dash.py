25ba914d30aa725883c77a8fe1d50977ff8394a9
kajiyagustavo <kajiyagustavo@gmail.com>
2025-01-05 18:51:39 -0300

    Aumento da memória de importação de arquivos

--- dash.py
+++ dash.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_option('server.maxUploadSize', 2048)  # Define o limite máximo para 2 GB
 import pandas as pd
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
